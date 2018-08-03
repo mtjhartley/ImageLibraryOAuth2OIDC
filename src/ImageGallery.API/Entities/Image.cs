b224a902efeb8e3ed9d2c6cb205c6882dcba44d6
mtjhartley <mtjhartley@gmail.com>
2018-08-02 17:41:18 -0700

    init commit

---
+++ src/ImageGallery.API/Entities/Image.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ImageGallery.API.Entities
+{
+    //init commit
+    public class Image
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string FileName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string OwnerId { get; set; }
+    }
+}
