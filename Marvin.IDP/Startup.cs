a789bdc53c1e6ab28d4ddf58ff544ef086315e60
mtjhartley <mtjhartley@gmail.com>
2018-08-02 18:19:36 -0700

    add frontend for identityserver with quickstart, configure startup.cs for mvc

--- Marvin.IDP/Startup.cs
+++ Marvin.IDP/Startup.cs
@@ -15,6 +15,10 @@ namespace Marvin.IDP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc();
+
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddTestUsers(Config.GetUsers())
@@ -30,6 +34,8 @@ namespace Marvin.IDP
                 app.UseDeveloperExceptionPage();
             }
             app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
             
         }
     }
